25480f99a376a83d17a5f33b6b091999dd873cd0
Vankudoth Sunil Kumar <vankudothsunilkumar366@gmail.com>
2026-08-10 02:16:18 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private:
+    bool check(string &s,int l,int r){
+        while(l<r){
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int n=s.size();
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(s[l]!=s[r]){
+               return check(s,l+1,r) || check(s,l,r-1);
+               //else return false;
+            }
+             l++;
+             r--;
+        }
+        return true;
+    }
+};
