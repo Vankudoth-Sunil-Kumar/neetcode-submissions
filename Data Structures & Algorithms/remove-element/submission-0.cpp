4d883396913f3697b0f3918bd77ce240a2910604
Vankudoth Sunil Kumar <vankudothsunilkumar366@gmail.com>
2026-08-09 13:40:58 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val) continue;
+            else ans.push_back(nums[i]);
+        }
+        nums=ans;
+        return nums.size();
+    }
+};
